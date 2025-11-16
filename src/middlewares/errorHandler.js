22751afaa93dc4291a9fb5c585ad6b27c01e892d
Baran Taşcı <barantasci44@gmail.com>
2025-11-17 02:32:02 +0300

    Ödev#4

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,8 +1,15 @@
 export const errorHandler = (err, req, res, next) => {
-  const status = err.status || 400;
+  if (err.isJoi || err.details) {
+    return res.status(400).json({
+      status: 400,
+      message: err.details[0].message,
+    });
+  }
+
+  const status = err.status || 500;
 
   res.status(status).json({
-    status: status,
-    message: err.message || "Bad Request",
+    status,
+    message: err.message || 'Internal Server Error',
   });
 };
