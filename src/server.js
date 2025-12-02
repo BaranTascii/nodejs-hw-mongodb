5a379cfdbac03c1089c4638352dbbb714274ffcd
Baran Taşcı <barantasci44@gmail.com>
2025-12-02 13:15:56 +0300

    Ödev#7

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ import authRouter from "./routers/auth.js";
 import contactsRouter from "./routers/contacts.js";
 
 import notFoundHandler from "./middlewares/notFoundHandler.js";
-import errorHandler from "./middlewares/errorHandler.js";
+import { errorHandler } from "./middlewares/errorHandler.js";
 
 const app = express();
 
