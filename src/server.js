4baa0c30c33da083b40f82554c40206f328e5a64
Baran Taşcı <barantasci44@gmail.com>
2025-12-03 17:15:56 +0300

    Ödev#7

--- src/server.js
+++ src/server.js
@@ -1,11 +1,14 @@
-import express from "express";
-import cors from "cors";
+import express from 'express';
+import cors from 'cors';
 
-import authRouter from "./routers/auth.js";
-import contactsRouter from "./routers/contacts.js";
+import authRouter from './routers/auth.js';
+import contactsRouter from './routers/contacts.js';
 
-import { notFoundHandler } from "./middlewares/notFoundHandler.js";
-import { errorHandler } from "./middlewares/errorHandler.js";
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
+import { errorHandler } from './middlewares/errorHandler.js';
+
+import swaggerUi from 'swagger-ui-express';
+import swaggerSpec from '../src/swagger/swagger.js';
 
 const app = express();
 
@@ -13,8 +16,15 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/api/auth", authRouter);
-app.use("/api/contacts", contactsRouter);
+app.use('/api/auth', authRouter);
+app.use('/api/contacts', contactsRouter);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+app.listen(3000, () => {
+  console.log('Server running on http://localhost:3000');
+  console.log('Swagger docs → http://localhost:3000/api-docs');
+});
 
 app.use(notFoundHandler);
 
--- src/swagger/swagger.js
+++ src/swagger/swagger.js
@@ -1,6 +1,19 @@
-import swaggerUi from "swagger-ui-express";
-import swaggerDoc from "../../docs/swagger.json" assert { type: "json" };
+import swaggerJSDoc from 'swagger-jsdoc';
+import path from 'path';
 
-export const swaggerRouter = (app) => {
-  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDoc));
+const __dirname = path.resolve();
+
+const options = {
+  definition: {
+    openapi: '3.1.0',
+    info: {
+      title: 'Contacts API',
+      version: '1.0.0',
+    },
+  },
+  apis: [path.join(__dirname, 'docs/openapi.yaml')],
 };
+
+const swaggerSpec = swaggerJSDoc(options);
+
+export default swaggerSpec;
